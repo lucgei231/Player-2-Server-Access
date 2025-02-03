f55c359cf2f8f9ef848186474f39b1644c07a848
Edvin Bryntesson <edvin@elefant.gg>
2025-02-03 15:15:20 +0100

    Init mod

---
+++ src/main/java/com/elefant/botAuthentication/BotAuthentication.java
@@ -0,0 +1,10 @@
+package com.elefant.botAuthentication;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BotAuthentication implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
