b92958f885dee31146c7529e6ce56be88915949b
Edvin Bryntesson <edvin@elefant.gg>
2025-02-03 16:09:32 +0100

    Split mixins

--- src/main/java/com/elefant/botAuthentication/mixin/server/LoginChecker.java
+++ src/main/java/com/elefant/botAuthentication/mixin/server/LoginChecker.java
@@ -1,4 +1,4 @@
-package com.elefant.botAuthentication.mixin;
+package com.elefant.botAuthentication.mixin.server;
 
 
 import net.minecraft.server.network.ServerLoginNetworkHandler;
@@ -18,7 +18,7 @@ public class LoginChecker {
 
     @Redirect(method = "onHello", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isOnlineMode()Z", opcode = Opcodes.GETFIELD))
     private boolean test(MinecraftServer instance) {
-        if (this.profileName != null && this.profileName.equals("test")) {
+        if (this.profileName != null && this.profileName.startsWith("Player")) {
             System.out.println("Elefant is trying to join the server!");
             return false;
         } else {
